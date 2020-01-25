6d86d54ec0a995e862838474477be42c48b13f0f
Darren Thompson <me@dtho.mp>
2020-01-25 00:18:33 -0500

    Make makefile NXDK-like

--- src/screen.h
+++ src/screen.h
@@ -11,6 +11,13 @@
  */
 #include "SDL.h"
 
+#ifdef __XBOX__
+#   include "glshim.h"
+#else
+#   include <GL/gl.h>
+#   include <GL/glu.h>
+#endif
+
 #define PAD_UP 1
 #define PAD_DOWN 2
 #define PAD_LEFT 4
@@ -20,37 +27,6 @@
 #define PAD_BUTTONP 64
 
 #define DEFAULT_BRIGHTNESS 224
-
-// TODO: HACKHACKHACKHACK
-#define GLfloat float
-#define GLuint int
-#define GL_TEXTURE_2D 1
-#define GL_TEXTURE_MAG_FILTER 1
-#define GL_TEXTURE_MIN_FILTER 1
-#define GL_TEXTURE_LINEAR 1
-#define GL_MODELVIEW 1
-#define GL_LINE_SMOOTH 1
-#define GL_SRC_ALPHA 1
-#define GL_PROJECTION 1
-#define GL_ONE 1
-#define GL_BLEND 1
-#define GL_LIGHTING 1
-#define GL_CULL_FACE 1
-#define GL_DEPTH_TEST 1
-#define GL_COLOR_MATERIAL 1
-#define GL_LINEAR 1
-#define GL_LINEAR_MIPMAP_NEAREST 1
-#define GL_RGB 1
-#define GL_UNSIGNED_BYTE 1
-#define SDL_OPENGL 1
-#define SDL_RESIZABLE 1
-#define SDL_FULLSCREEN 1
-#define GL_COLOR_BUFFER_BIT 1
-#define GL_TRIANGLE_FAN 1
-#define GL_LINE_LOOP 1
-#define GL_LINES 1
-#define GL_QUADS 1
-#define GL_LINE_STRIP 1
 
 extern float eyeX, eyeY, eyeZ;
 extern float pitch, roll;
